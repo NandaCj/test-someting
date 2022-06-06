946d79fca1ec7fc0a5450be55a2cdd373f51d9db
nandha <nandha.paramasivam@gmail.com>
2022-06-06 23:03:29 +0530

    new

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ def print_hi(name):
 def new_hello_world():
     print("Hi There")
 
+def new_hello_world():
+    print("Hi There")
+
+def new_hello_world():
+    print("Hi There")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
